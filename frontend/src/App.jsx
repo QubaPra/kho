db2ffa83f11116f0ab4abbac210587e180ee6db1
Quba <playquba@gmail.com>
2025-02-19 21:40:34 +0100

    dark mode dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,63 @@ import { useState, useEffect } from 'react'
 
 function App() {
     
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      return savedTheme === 'dark';
+    } else {
+      const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return prefersDark;
+    }
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+
   return (
-    <div class="bg-gray-100 dark:bg-gray-900 min-h-screen">
+    <div class="bg-gray-100 dark:bg-black min-h-screen dark:text-gray-100 text-gray-800">
     
-    <header class="bg-white shadow-sm">
+    <header class="bg-white dark:bg-gray-900 shadow-sm">
         <div
           class="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between"
         >
           <div class="flex items-center space-x-2">
-            <button class="text-xl font-semibold text-gray-800">eKapituła HKK</button>
+            <button class="text-xl font-semibold  dark:text-gray-100">eKapituła HKK</button>
           </div>
           <div class="max-w-7xl mx-auto px-4 py-2 flex items-center space-x-6">
-            <button class="text-sm font-medium text-gray-800 hover:text-blue-800">
+            <button class="text-sm font-medium  hover:text-blue-800  dark:hover:text-blue-300">
               Użytkownicy
             </button>
-            <button class="text-sm font-medium text-gray-800 hover:text-blue-800">
+            <button class="text-sm font-medium  hover:text-blue-800  dark:hover:text-blue-300">
               Wszystkie próby
             </button>
           </div>
           <div class="flex items-center space-x-2">
           <button
-              class="material-symbols-outlined text-white bg-gray-900 p-2 rounded-lg"              
+              class="material-symbols-outlined bg-gray-900 dark:bg-gray-100 dark:text-gray-800 text-gray-100 p-2 rounded-lg"
+              onClick={toggleDarkMode}              
             >
-              dark_mode
+              {isDarkMode ? 'light_mode' : 'dark_mode'}
             </button>
             <button
-              class="material-symbols-outlined text-white bg-green-500 p-2 rounded-lg"
+              class="material-symbols-outlined bg-green-500 p-2 rounded-lg"
             >
               person
             </button>
   
             <button
-              class="material-symbols-outlined text-white cursor-pointer bg-red-500 p-2 rounded-lg"
+              class="material-symbols-outlined  bg-red-500 p-2 rounded-lg"
             >
               logout
             </button>
@@ -43,28 +68,28 @@ function App() {
 
         
       <main class="max-w-7xl mx-auto px-4 py-6">
-        <div class="bg-white rounded-lg shadow p-6 mb-6">
+        <div class="bg-white dark:bg-gray-900 rounded-lg shadow p-6 mb-6">
           <div class="flex items-center justify-between mb-2">
-            <h2 class="text-2xl font-semibold text-gray-800">
+            <h2 class="text-2xl font-semibold">
               wyw. Jakub Prażuch próba na stopień HO
             </h2>
             <div class="flex space-x-2">
-                <button class="flex items-center bg-gray-200 p-2 rounded-lg hover:bg-gray-300 cursor-pointer">                  
+                <button class="flex items-center bg-gray-200 p-2 rounded-lg hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800">                  
                   <span class="material-symbols-outlined">
                   list_alt_check
                   </span>
                   <span class="ml-2">Zgłoś próbę do opiekuna</span>
                 </button>
-                <button class="cursor-pointer">
+                <button>
                   <span
-                    class="material-symbols-outlined bg-gray-200 p-2 rounded-lg hover:bg-gray-300"
+                    class="material-symbols-outlined bg-gray-200 p-2 rounded-lg hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800"
                   >
                     edit_square
                   </span>
                 </button>
-                <button class="cursor-pointer">
+                <button >
                   <span
-                    class="material-symbols-outlined bg-red-300 p-2 rounded-lg hover:bg-red-400"
+                    class="material-symbols-outlined bg-red-300 p-2 rounded-lg hover:bg-red-400 dark:bg-red-700 dark:hover:bg-red-800"
                   >
                     delete
                   </span>
@@ -73,11 +98,11 @@ function App() {
               
           </div>
           <div class="flex space-x-4">
-            <div class="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
+            <div class="bg-blue-100 text-blue-800 dark:bg-blue-800 dark:text-blue-100 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
               <p class="font-semibold">Stan:</p>
               <span>do akceptacji przez opiekuna</span>
             </div>
-            <div class="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
+            <div class="bg-blue-100 text-blue-800 dark:bg-blue-800 dark:text-blue-100 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
               <p class="font-semibold">Data zakończenia:</p>
               <span>marzec 2026</span>
             </div>
@@ -87,23 +112,23 @@ function App() {
           
           <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
             <div>
-                <p class="text-sm text-gray-500">Email do kontaktu</p>
+                <p class="text-sm text-gray-400">Email do kontaktu</p>
                 <p class="font-medium">jakub.prazuch@malopolska.zhr.pl</p>
             </div>
             <div>
-                <p class="text-sm text-gray-500">Email opiekuna</p>
+                <p class="text-sm text-gray-400">Email opiekuna</p>
                 <p class="font-medium">andrzej.zarnowski@malopolska.zhr.pl</p>
             </div>
             <div>
-                <p class="text-sm text-gray-500">Data urodzenia</p>
+                <p class="text-sm text-gray-400">Data urodzenia</p>
                 <p class="font-medium">25 lipca 2005</p>
             </div>
             <div>
-                <p class="text-sm text-gray-500">Imię i nazwisko opiekuna</p>
+                <p class="text-sm text-gray-400">Imię i nazwisko opiekuna</p>
                 <p class="font-medium">Andrzej Żarnowski</p>
             </div>
             <div>
-                <p class="text-sm text-gray-500">Drużyna</p>
+                <p class="text-sm text-gray-400">Drużyna</p>
                 <p class="font-medium">40 KDH Barykada</p>
             </div>          
         </div>
@@ -111,7 +136,7 @@ function App() {
         <div class="mt-12">
             <h3 class="text-xl font-medium mb-4">Zadania</h3>
             <table class="w-full">
-                <thead class="bg-gray-50 text-left text-sm">
+                <thead class="bg-gray-50 dark:bg-gray-700 text-left text-sm">
                     <tr>
                         <th class="p-3">Lp</th>
                         <th class="p-3">Treść zadania</th>
@@ -120,7 +145,7 @@ function App() {
                         <th class="p-3">Edycja</th>
                     </tr>
                 </thead>
-                <tbody class="divide-y divide-gray-200">
+                <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
                     <tr>
                         <td class="p-3">1</td>
                         <td class="p-3 max-w-[300px]">
@@ -128,20 +153,20 @@ function App() {
                         </td>
                         <td class="p-3 flex-column space-y-2">
                             
-                            <div class="bg-blue-200 text-blue-900 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
+                            <div class="bg-blue-200 text-blue-900 dark:bg-blue-800 dark:text-blue-200 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
                                 <span class="material-symbols-outlined">school</span>
                                 <span>Rozwój intelektualny i zawodowy</span>
                               </div>
 
-                              <div class="bg-purple-200 text-purple-900 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
+                              <div class="bg-purple-200 text-purple-900 dark:bg-purple-800 dark:text-purple-200 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
                                 <span class="material-symbols-outlined">sentiment_very_satisfied</span>
                                 <span>Praca nad charakterem</span>
                               </div>
 
-                              <button class="bg-gray-200 text-gray-900 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
-                                <span class="material-symbols-outlined">add</span>
+                              
+                                <button class="material-symbols-outlined bg-gray-200 dark:bg-gray-600 px-3 py-1 rounded-full">add</button>
                                 
-                              </button>
+                              
 
                         </td>
                         <td class="p-3">
@@ -151,8 +176,8 @@ function App() {
 
                         </td>
                         <td class="p-3">
-                            <span class="material-symbols-outlined text-gray-400 hover:text-gray-600 cursor-pointer">edit</span>
-                            <span class="material-symbols-outlined text-gray-400 hover:text-red-600 cursor-pointer">delete</span>
+                            <button class="material-symbols-outlined text-gray-400 hover:text-gray-600">edit</button>
+                            <button class="material-symbols-outlined text-gray-400 hover:text-red-600">delete</button>
                         </td>
                     </tr>
                     <tr>
@@ -162,15 +187,12 @@ function App() {
                         </td>
                         <td class="p-3 flex-column space-y-2">
 
-                            <div class="bg-pink-200 text-pink-900 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
+                            <div class="bg-pink-200 text-pink-900 dark:bg-pink-800 dark:text-pink-200 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
                                 <span class="material-symbols-outlined">diversity_4</span>
                                 <span>Powołanie, wychowanie prorodzinne</span>
                               </div>
 
-                              <button class="bg-gray-200 text-gray-900 px-3 py-1 rounded-full text-sm w-fit flex items-center space-x-1">
-                                <span class="material-symbols-outlined">add</span>
-                                
-                              </button>
+                              <button class="material-symbols-outlined bg-gray-200 dark:bg-gray-600 px-3 py-1 rounded-full">add</button>
 
 
                         </td>
@@ -180,8 +202,8 @@ function App() {
 
                         </td>
                         <td class="p-3">
-                            <span class="material-symbols-outlined text-gray-400 hover:text-gray-600 cursor-pointer">edit</span>
-                            <span class="material-symbols-outlined text-gray-400 hover:text-red-600 cursor-pointer">delete</span>
+                            <button class="material-symbols-outlined text-gray-400 hover:text-gray-600">edit</button>
+                            <button class="material-symbols-outlined text-gray-400 hover:text-red-600">delete</button>
                         </td>
                     </tr>
                 </tbody>
@@ -197,22 +219,22 @@ function App() {
             <h3 class="text-xl font-medium">Komentarze</h3>
             <div class="space-y-4">
                 <div class="space-y-2">
-                    <p class="text-sm text-gray-500">23 lipca 2024</p>
-                    <div class="bg-gray-50 p-3 rounded-lg">
+                    <p class="text-sm text-gray-500 dark:text-gray-400">23 lipca 2024</p>
+                    <div class="bg-gray-50 dark:bg-gray-700 p-3 rounded-lg">
                         <p class="font-medium">Andrzej Żarnowski:</p>
                         <p>Jak dla mnie jest w porządku! :)</p>
                     </div>
                 </div>
-                <div class="border-t border-gray-200 pt-4">
+                <div class="border-t border-gray-200 dark:border-gray-700 pt-4">
                     <div class="flex items-center space-x-2">
                         <input 
                             type="text" 
                             placeholder="Twój komentarz" 
-                            class="flex-1 p-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
+                            class="flex-1 p-2 border border-gray-200 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200 dark:focus:ring-blue-600"
                         />
-                        <button class="text-blue-600 hover:text-blue-800">
-                            <span class="material-symbols-outlined">send</span>
-                        </button>
+                        
+                            <button class="material-symbols-outlined text-blue-600 hover:text-blue-800">send</button>
+                        
                     </div>
                 </div>
             </div>
